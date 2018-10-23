fad1a87829c7e613604b4288a0a3989744b44753
KQ <krq205@gmail.com>
2018-10-22 22:21:37 -0700

    added functionality of buttons, dynamically created buttons, gifs can stop and start. Need to make prettier

--- assets/javascript/swag.js
+++ assets/javascript/swag.js
@@ -7,15 +7,27 @@ function generateInitialButtons(){
             
             var butt = $("<button>");
                 butt.attr("data-animal", animalText);
+                butt.attr("class","button-entry");
                 butt.text(animalText);
-            console.log(butt.attr("data-animal"));
             $("#butt-holder").append(butt);
         }
 }
 
-function buttonEventHandler () {
-$("button").on("click", function() {
+$("#find-giph").on("click", function(){
     console.log("received");
+    var userInput = $("#search").val();
+    var butt = $("<button>");
+        butt.attr("data-animal", userInput);
+        butt.attr("class","button-entry");
+        butt.text(userInput);
+    $("#butt-holder").append(butt);
+    $("#search").val("");
+    buttonEventHandler();
+})
+
+function buttonEventHandler () {
+
+$(".button-entry").on("click", function() {
       var animal = $(this).attr("data-animal");
       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         animal + "&api_key=dc6zaTOxFJmzC&limit=10";
@@ -34,15 +46,33 @@ $("button").on("click", function() {
           p.text("Rating: " + results[i].rating)
           var animalImage = $("<img>"); 
             animalImage.attr("src", results[i].images.fixed_height.url);
+            animalImage.attr("data-still", results[i].images.original_still.url);
+            animalImage.attr("data-animate", results[i].images.fixed_height.url);
+            animalImage.attr("class", "gif");
+            animalImage.attr("data-state","animate");
             animalDiv.append(p);
             animalDiv.append(animalImage);
             $("#gifs-appear-here").prepend(animalDiv);
         }
+        $(".gif").on("click", function() {
+            var state = $(this).attr("data-state");
+        
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            }
+        })
     });
+
 })
 }
+
 
 $(document).ready(function() {
     generateInitialButtons();
     buttonEventHandler();
+
 })
